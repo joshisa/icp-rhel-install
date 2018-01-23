f3946357e0c15da9991c7b55f79f8b2a5baef733
Sanjay Joshi <joshisa@us.ibm.com>
2018-01-23 17:29:29 +0000

    Added an umbrella script

---
+++ scripts/finis_coronat_opus.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+#set -x
+
+echo "One stop shop script for deploying ICP if you have line-of-sight to all IPs for the cluster"
+./00-variables.sh
+./01-update-hosts.sh
+./01-1-passwordless-ssh.sh
+./01-2-bind-mounts.sh
+./02-ssh-setup.sh
+./03-install-packages.sh
+./04-configure-os.sh
+./05-firewall-config.sh
+./06-get-installer.sh
+./07-configure-installer.sh
+./08-install.sh
+./09-kubeconfig.sh
