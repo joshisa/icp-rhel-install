acd8a44a122462afda0e9f70f4d91dfb94c06db0
Sanjay Joshi <joshisa@us.ibm.com>
2018-01-25 18:43:46 +0000

    Added an optional reset-cluster script

---
+++ scripts/12-reset-cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------------------------------\\
+# Description:
+#   A basic reset of the cluster and its workers for RHEL 7.4 or Ubuntu 16.04
+# ----------------------------------------------------------------------------------------------------\\
+# Get the variables
+source 00-variables.sh
+
+sudo systemctl restart docker
+echo "Looping through workers ..."
+for ((i=0; i < $NUM_WORKERS; i++)); do
+    ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo systemctl restart docker
+done
+
