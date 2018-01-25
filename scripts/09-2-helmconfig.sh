b3808a3f3795262cbb8a1cf634b18049fbda535f
Sanjay Joshi <joshisa@us.ibm.com>
2018-01-25 17:27:54 -0500

    Create 09-2-helmconfig.sh

---
+++ scripts/09-2-helmconfig.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+HELMVERSION=2.7.2
+curl -Lo /tmp/helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v${HELMVERSION}-linux-amd64.tar.gz
+tar -zxvf /tmp/helm.tar.gz -C /tmp
+sudo mv /tmp/linux-amd64/helm /usr/local/bin/helm
+chmod +x /usr/local/bin/helm
+
+helm init —upgrade
+helm repo add ibm-charts https://raw.githubusercontent.com/IBM/charts/master/repo/stable/
