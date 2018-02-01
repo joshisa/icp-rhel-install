f5b944bd959ee9835e366a15711e8d15c25ce873
Sanjay Joshi <joshisa@us.ibm.com>
2018-02-01 17:37:35 +0100

    Update finis_coronat_opus.sh

--- scripts/finis_coronat_opus.sh
+++ scripts/finis_coronat_opus.sh
@@ -6,7 +6,6 @@ set -u
 
 clear
 echo "One stop shop script for deploying ICP if you have line-of-sight to all IPs for the cluster"
-./01-0-update-hosts.sh
 ./01-1-passwordless-ssh.sh
 ./01-2-bind-mounts.sh
 ./02-ssh-setup.sh
@@ -18,4 +17,5 @@ echo "One stop shop script for deploying ICP if you have line-of-sight to all IP
 ./08-install.sh
 ./09-0-kubeconfig.sh
 ./09-1-setup-image-reg-pulls.sh
+./09-2-helmconfig.sh
 ./10-waiter.sh
