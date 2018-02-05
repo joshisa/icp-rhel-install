79937eed6694f38e40c91d7fd3f8e61c85f6e159
Ubuntu <ubuntu@om-demo-master.openstacklocal>
2018-02-05 09:57:06 +0000

    Added script for cli bluemix and icp plugin setup

---
+++ scripts/09-4-clibluemixconfig.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#  Purpose
+#  This scripts sets up the ETCDCTL CLI
+#  Tested on Ubuntu 16.04 x86 system.
+#
+
+set -e
+set -u
+shopt -s expand_aliases
+
+source 00-variables.sh
+BLUEMIXCLIVERSION=0.6.4
+curl -Lo /tmp/clibluemix.tar.gz https://clis.ng.bluemix.net/download/bluemix-cli/${BLUEMIXCLIVERSION}/linux64
+tar -zxf /tmp/clibluemix.tar.gz -C /tmp
+sudo /tmp/Bluemix_CLI/install_bluemix_cli
+
+curl --insecure -Lo /tmp/icpcli https://${PUBLIC_IP}:8443/api/cli/icp-linux-amd64
+bx plugin install /tmp/icpcli
+
+# Cleanup
+sudo rm /tmp/clibluemix.tar.gz
+sudo rm -rf /tmp/Bluemix_CLI
+sudo rm /tmp/icpcli
+
+bx plugin show icp
+echo ""
+bx pr login -a https://${PUBLIC_IP}:8443 --skip-ssl-validation -u admin -p admin -c id-icp-account
+echo ""
+bx pr
