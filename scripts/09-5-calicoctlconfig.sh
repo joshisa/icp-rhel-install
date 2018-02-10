3a81c386f51a72a96de746be51c01ef3598e20a1
Ubuntu <ubuntu@om-demo-master.openstacklocal>
2018-02-10 00:43:53 +0000

    Updated installer to prep for federation, calicoctl setup and diagnostics for calicoctl

---
+++ scripts/09-5-calicoctlconfig.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#  Purpose
+#  This scripts sets up the CALICOCTL CLI
+#  Tested on Ubuntu 16.04 x86 system.
+#
+
+set -e
+set -u
+shopt -s expand_aliases
+
+source 00-variables.sh
+
+CALICOCTLVERSION=1.4.0
+CALICOCTL_CFG_PATH=/etc/calico/calicoctl.cfg
+sudo rm -rf /etc/calico
+sudo mkdir -p /etc/calico
+
+curl -Lo /tmp/calicoctl https://github.com/projectcalico/calicoctl/releases/download/v${CALICOCTLVERSION}/calicoctl
+chmod +x /tmp/calicoctl
+sudo mv /tmp/calicoctl /usr/local/bin/calicoctl
+
+
+ip=$(ps -ef | grep -Po "(?<=advertise-client-urls\=)https:\/\/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:4001");
+#echo "${ip} is your etcd endpoint(s)"
+
+# Build config file for calicoctl
+touch calicoctl.cfg
+echo "apiVersion: v1" >> calicoctl.cfg
+echo "kind: calicoApiConfig" >> calicoctl.cfg
+echo "metadata:" >> calicoctl.cfg
+echo "spec:" >> calicoctl.cfg
+#echo "  etcdEndpoints: ${ip}" >> calicoctl.cfg
+#echo "  etcdKeyFile: /etc/cfc/conf/etcd/client-key.pem" >> calicoctl.cfg
+#echo "  etcdCertFile: /etc/cfc/conf/etcd/client.pem" >> calicoctl.cfg
+#echo "  etcdCACertFile: /etc/cfc/conf/etcd/ca.pem" >> calicoctl.cfg
+echo "  datastoreType: \"kubernetes\"" >> calicoctl.cfg
+echo "  kubeconfig: \"/home/${SSH_USER}/.kube/config\"" >> calicoctl.cfg
+
+sudo mv calicoctl.cfg /etc/calico
+
+echo "Executing command:  calicoctl get nodes -o wide"
+calicoctl get nodes -o wide
+echo ""
+echo "Executing command:  sudo calicoctl node status"
+sudo calicoctl node status
+echo ""
+echo "Executing command:  calicoctl get pool"
+calicoctl get pool
+echo ""
+echo "Executing command:  calicoctl get wep"
+calicoctl get wep
+echo ""
+echo "Executing command:  calicoctl get profile"
+calicoctl get profile
+echo ""
+echo "Executing command:  calicoctl get policy"
+calicoctl get policy
+echo ""
