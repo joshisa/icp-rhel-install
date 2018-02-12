b6dc0f746606e48ce6a3fd4fd51d36151949cbd0
Sanjay Joshi <joshisa@us.ibm.com>
2018-02-12 02:52:52 -0500

    Update 13-6-load-cloud-automation-manager.sh

--- scripts/13-6-load-cloud-automation-manager.sh
+++ scripts/13-6-load-cloud-automation-manager.sh
@@ -12,8 +12,9 @@ rm -rf /tmp/cam
 mkdir -p /tmp/cam
 
 CAM_TARBALL_PATH=https://path/to/some.tar.gz
-wget -O /tmp/cam/cam.tar.gz ${CAM_TARBALL_PATH} # --user <OPTIONAL> --password <OPTIONAL>
-bx pr load-ppa-archive --namespace services --archive /tmp/cam/cam.tar.gz
+CAM_FILE=${CAM_TARBALL_PATH##*/}
+wget -O /tmp/cam/${CAM_FILE} ${CAM_TARBALL_PATH} # --user <OPTIONAL> --password <OPTIONAL>
+bx pr load-ppa-archive --namespace services --archive /tmp/cam/${CAM_FILE}
 
 echo -e "${beers}   Congrats.  After synchronizing your cluster repositories, you should see the CAM chart available within the catalog"
 # Cleanup
