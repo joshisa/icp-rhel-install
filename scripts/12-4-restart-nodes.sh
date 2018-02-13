b5fd12bb87985b51d7fcc9bd35bc934bd2b7675b
root <root@am-k8mast.rtp.raleigh.ibm.com>
2018-02-12 22:10:27 -0500

    Added node restart script

---
+++ scripts/12-4-restart-nodes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------------------------------\\
+# Description:
+#   A basic reset of the cluster and its workers for RHEL 7.4 or Ubuntu 16.04
+# ----------------------------------------------------------------------------------------------------\\
+# Get the variables
+source 00-variables.sh
+
+echo "Looping through workers ..."
+for ((i=0; i < $NUM_WORKERS; i++)); do
+    ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo shutdown -r --now
+done
+
+echo "Looping through proxies ..."
+for ((i=0; i < $NUM_PROXIES; i++)); do
+    ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} sudo shutdown -r --now
+done
+
+echo "Looping through management ..."
+for ((i=0; i < $NUM_MANAGERS; i++)); do
+    ssh ${SSH_USER}@${MANAGEMENT_HOSTNAMES[i]} sudo shutdown -r --now
+done
+
