84a74d1ece06f794106944cd0021ca3afc1a529e
root <root@am-k8mast.rtp.raleigh.ibm.com>
2018-02-12 22:13:00 -0500

    updated restart nodes

--- scripts/12-4-restart-nodes.sh
+++ scripts/12-4-restart-nodes.sh
@@ -8,16 +8,16 @@ source 00-variables.sh
 
 echo "Looping through workers ..."
 for ((i=0; i < $NUM_WORKERS; i++)); do
-    ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo shutdown -r --now
+    ssh ${SSH_USER}@${WORKER_HOSTNAMES[i]} sudo /sbin/shutdown -r now
 done
 
 echo "Looping through proxies ..."
 for ((i=0; i < $NUM_PROXIES; i++)); do
-    ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} sudo shutdown -r --now
+    ssh ${SSH_USER}@${PROXY_HOSTNAMES[i]} sudo /sbin/shutdown -r now
 done
 
 echo "Looping through management ..."
 for ((i=0; i < $NUM_MANAGERS; i++)); do
-    ssh ${SSH_USER}@${MANAGEMENT_HOSTNAMES[i]} sudo shutdown -r --now
+    ssh ${SSH_USER}@${MANAGEMENT_HOSTNAMES[i]} /sbin/shutdown -r now
 done
 
