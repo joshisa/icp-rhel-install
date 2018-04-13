a34e8921630dfca0289313d9f0be9a2348c11eb0
Sanjay Joshi <joshisa@us.ibm.com>
2018-04-13 09:50:37 -0400

    Update to 0.6.6

--- scripts/09-4-clibluemixconfig.sh
+++ scripts/09-4-clibluemixconfig.sh
@@ -9,7 +9,7 @@ set -u
 shopt -s expand_aliases
 
 source 00-variables.sh
-BLUEMIXCLIVERSION=0.6.4
+BLUEMIXCLIVERSION=0.6.6
 curl -Lo /tmp/clibluemix.tar.gz https://clis.ng.bluemix.net/download/bluemix-cli/${BLUEMIXCLIVERSION}/linux64
 tar -zxf /tmp/clibluemix.tar.gz -C /tmp
 sudo /tmp/Bluemix_CLI/install_bluemix_cli
