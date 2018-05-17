52984d5d383670e5e36551d73217b38aa5c2f6ed
Sanjay Joshi <joshisa@us.ibm.com>
2018-05-17 00:55:30 -0400

    Patching ClusterRole for privileged access
    Makes the deploy compatible in ICP 2.1.0.3

--- scripts/13-5-deploy-weave-scope.sh
+++ scripts/13-5-deploy-weave-scope.sh
@@ -11,6 +11,8 @@ source 00-variables.sh
 kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 ./10-waiter.sh "pods" "weave" "0/1"
 
+kubectl patch ClusterRoles weave-scope -n weave -p '{"rules":[{"apiGroups":["extensions"],"resourceNames":["privileged"],"resources":["podsecuritypolicies"],"verbs":["use"]}]}'
+
 # Change svc from ClusterIP to NodePort
 kubectl get svc weave-scope-app -n weave -o yaml | sed 's/ClusterIP/NodePort/' | kubectl replace -f -
 PORT=$(kubectl get svc weave-scope-app -n weave -o jsonpath='{.spec.ports[*].nodePort}')
