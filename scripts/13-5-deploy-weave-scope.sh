1e288c8fe74c58a4130dea6aa468544fe073541b
Sanjay Joshi <joshisa@us.ibm.com>
2018-05-17 00:59:47 -0400

    Re-arranging sequence for clusterrole patch

--- scripts/13-5-deploy-weave-scope.sh
+++ scripts/13-5-deploy-weave-scope.sh
@@ -9,9 +9,9 @@ set -e
 source 00-variables.sh
 
 kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-./10-waiter.sh "pods" "weave" "0/1"
-
 kubectl patch ClusterRoles weave-scope -n weave -p '{"rules":[{"apiGroups":["extensions"],"resourceNames":["privileged"],"resources":["podsecuritypolicies"],"verbs":["use"]}]}'
+
+./10-waiter.sh "pods" "weave" "0/1"
 
 # Change svc from ClusterIP to NodePort
 kubectl get svc weave-scope-app -n weave -o yaml | sed 's/ClusterIP/NodePort/' | kubectl replace -f -
