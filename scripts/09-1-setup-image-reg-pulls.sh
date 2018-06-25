2dcb0d8c784e9c62982c5f1b3c43e5005bbb2c89
Sanjay Joshi <joshisa@us.ibm.com>
2018-06-25 17:00:23 -0400

    Update 09-1-setup-image-reg-pulls.sh

--- scripts/09-1-setup-image-reg-pulls.sh
+++ scripts/09-1-setup-image-reg-pulls.sh
@@ -5,5 +5,5 @@ set -u
 
 source 00-variables.sh
 
-kubectl create secret docker-registry myregistrykey --namespace hygieia --docker-server=mycluster.icp:8500 --docker-username="${ICPUSER}" --docker-password="${ICPPW}" --docker-email="${ICPEMAIL}"
-kubectl patch serviceaccount default --namespace hygieia -p '{"imagePullSecrets": [{"name": "myregistrykey"}]}'
+kubectl create secret docker-registry myregistrykey --namespace default --docker-server=mycluster.icp:8500 --docker-username="${ICPUSER}" --docker-password="${ICPPW}" --docker-email="${ICPEMAIL}"
+kubectl patch serviceaccount default --namespace default -p '{"imagePullSecrets": [{"name": "myregistrykey"}]}'
